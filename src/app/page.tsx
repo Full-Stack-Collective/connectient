7e49987e9fae84f1370783c604ea7e2cbe12eb0d
Devin Jaggernauth <neexis@gmail.com>
2023-06-13 09:06:35 -0400

    remove tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
+import styles from './styles.module.css'
 
 export default function Home() {
   return (
-    <main className="min-h-[95vh] p-24 m-auto flex flex-col items-center">
+    <main className={styles.main}>
 
-    <h1 className='text-3xl font-bold'>Let&apos;s get started</h1>
+    <h1>Let&apos;s get started</h1>
     <p>The blank canvas upon which we shall paint</p>
     </main>
   )
