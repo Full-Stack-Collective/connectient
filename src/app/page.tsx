7bc688c1e025471a246955dbdb090ca66debdbcb
Shraddha <mailmeatshraddha@gmail.com>
2023-06-13 11:30:41 -0400

    run prettier fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,8 @@ import styles from './styles.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-
-    <h1>Let&apos;s get started</h1>
-    <p>The blank canvas upon which we shall paint</p>
+      <h1>Let&apos;s get started</h1>
+      <p>The blank canvas upon which we shall paint</p>
     </main>
   )
 }
