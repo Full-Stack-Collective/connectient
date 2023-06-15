a6b5c01d381cb55081e99d66378f841a2655b5ec
Devin Jaggernauth <neexis@gmail.com>
2023-06-14 22:25:46 -0400

    create test component

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,14 @@
 const path = require('path')
- 
-const buildEslintCommand = (filenames) =>
+
+const buildEslintCommand = (filenames) => [
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`
- 
+    .join(' --file ')}`,
+  `prettier --write ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`,
+]
+
 module.exports = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
 }
