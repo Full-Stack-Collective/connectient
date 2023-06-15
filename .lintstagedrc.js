f47ef951bf84982b637ee4eac9d9da0bc9ad7e69
Devin Jaggernauth <neexis@gmail.com>
2023-06-14 22:42:03 -0400

    update lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,29 @@
 const path = require('path')
 
-const buildEslintCommand = (filenames) => [
+const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`,
-  `prettier --write ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`,
-]
+    .join(' --file ')}`
+
+const formatCommand = 'prettier --write'
 
 module.exports = {
   '*.{js,jsx,ts,tsx,css}': [buildEslintCommand],
+  '!*.{js,jsx,ts,tsx,css,scss}': [formatCommand],
 }
+
+// /* eslint-env node */
+// const path = require('path');
+
+// const eslintCommand = (filenames) =>
+//   `next lint --file ${filenames
+//     .map((f) => path.relative(process.cwd(), f))
+//     .join(' --file ')}`;
+
+// const formatCommand = 'prettier --write';
+// const stylelintCommand = 'stylelint --allow-empty-input "**/*.{css,scss}"';
+// module.exports = {
+//   '*.{js,jsx,ts,tsx}': [formatCommand, eslintCommand],
+//   '*.{css,scss}': [formatCommand, stylelintCommand],
+//   '!*.{js,jsx,ts,tsx,css,scss}': [formatCommand],
+// };
