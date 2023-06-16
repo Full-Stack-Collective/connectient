7b919fca9a5919fcb69fa40aca3cb2d8279fde10
Shraddha <mailmeatshraddha@gmail.com>
2023-06-16 15:24:52 -0400

    add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from '@styles/footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>© Connectient {new Date().getFullYear()}</p>
+    </footer>
+  )
+}
+
+export default Footer
