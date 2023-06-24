a6b184982da47b9452b5118c1606cd2d3b55cdfd
Shraddha <mailmeatshraddha@gmail.com>
2023-06-24 10:40:47 -0400

    add login form component

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,92 @@
+'use client'
+
+import { useTransition } from 'react'
+import { useForm } from 'react-hook-form'
+import { loginFormAction, type LoginFormData } from './actions'
+import styles from '@styles/loginform.module.css'
+
+const LoginForm = () => {
+  const [isPending, startTransition] = useTransition()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<LoginFormData>({
+    defaultValues: {
+      userName: '',
+      userPassword: '',
+    },
+    mode: 'onChange',
+  })
+
+  const onSubmit = handleSubmit((data) => {
+    // Log in browser
+    console.log(data)
+    startTransition(() => {
+      loginFormAction(data)
+    })
+  })
+
+  console.log(errors)
+
+  return (
+    <form
+      className={styles.loginForm}
+      onSubmit={(event) => void onSubmit(event)}
+    >
+      <div className={styles.loginFormSection}>
+        <label className={styles.inputLabel} htmlFor="userName">
+          Username
+        </label>
+        <input
+          className={styles.input}
+          type="text"
+          id="userName"
+          placeholder="Enter username"
+          {...register('userName', {
+            required: 'Username is required.',
+            minLength: {
+              value: 4,
+              message: 'Username should be atleast 4 chars long',
+            },
+          })}
+        />
+        <p className={styles.error}>
+          {errors.userName && errors.userName.message}
+        </p>
+      </div>
+      <div className={styles.loginFormSection}>
+        <label className={styles.inputLabel} htmlFor="userPassword">
+          Password
+        </label>
+        <input
+          className={styles.input}
+          type="password"
+          id="userPassword"
+          placeholder="Enter password"
+          {...register('userPassword', {
+            required: 'Password is required.',
+            minLength: {
+              value: 8,
+              message: 'Password should be atleast 8 chars long',
+            },
+          })}
+        />
+        <p className={styles.error}>
+          {errors.userPassword && errors.userPassword.message}
+        </p>
+      </div>
+      <div className={styles.loginFormSection}>
+        <button
+          className={styles.submitButton}
+          type="submit"
+          // disabled={!isValid}
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  )
+}
+
+export default LoginForm
