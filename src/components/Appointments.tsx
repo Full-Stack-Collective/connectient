9cabb872da10bf6a2f884792cfa3c48f780bb2f5
Devin Jaggernauth <neexis@gmail.com>
2023-06-26 15:19:57 -0400

    make get request to appointments table

--- src/components/Appointments.tsx
+++ src/components/Appointments.tsx
@@ -5,22 +5,51 @@ interface Appointment {
   first_name: string
   last_name: string
   email: string
+  mobile_phone: string
+  requested_date: string
 }
 
 const Appointments = async () => {
   const { data: appointments } = await supabase.from('Appointments').select()
-  console.log('Appointments', appointments)
 
   return (
     <div>
       <h2>Appointments</h2>
-      {appointments?.map((appointment: Appointment) => (
-        <div key={appointment.id}>
-          <p>{appointment?.email}</p>
-          <p>{appointment.first_name}</p>
-          <p>{appointment.last_name}</p>
-        </div>
-      ))}
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Requested Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {appointments?.map((appointment: Appointment) => {
+            const {
+              id,
+              first_name,
+              last_name,
+              email,
+              mobile_phone,
+              requested_date,
+            } = appointment
+
+            const date = new Date(requested_date).toDateString()
+
+            return (
+              <tr key={id}>
+                <td>{first_name}</td>
+                <td>{last_name}</td>
+                <td>{email}</td>
+                <td>{mobile_phone}</td>
+                <td>{date}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   )
 }
