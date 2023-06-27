760be9865c52952e00bdab74823eb3ab01e9163c
nuriya <nuriya_a@yahoo.com>
2023-06-26 22:50:59 -0400

    created landing page

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import styles from '@styles/landingPage.module.css'
+import dentalScheduling from '@public/dentalScheduling.png'
+import Image from 'next/image'
+
+const LandingPage = () => {
+  return (
+    <div className={styles.container}>
+      <Image
+        src={dentalScheduling}
+        alt="image of calendar and fake dentures"
+        className={styles.img}
+      />
+      <h1 className={styles.header}>Welcome to Essential Dental Solutions</h1>
+      <p>This is an online appointment request system.</p>
+      <p>
+        If you are ready, click on &quot;Get Started&quot; below to proceed.
+      </p>
+      <Link href="/appointments">
+        <button className={styles.button}>Get Started</button>
+      </Link>
+    </div>
+  )
+}
+export default LandingPage
