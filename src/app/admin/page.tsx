77518c05a088143a5ac53b55352091c6f4654374
Devin Jaggernauth <neexis@gmail.com>
2023-06-27 21:34:02 -0400

    clean up test component

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,6 @@
 import styles from '../styles.module.css'
 import test from '@styles/test.module.css'
 import LoginForm from '@/components/LoginForm'
-import Appointments from '@/components/Appointments'
 
 export default function Admin() {
   return (
@@ -9,7 +8,6 @@ export default function Admin() {
       <h1 className={test.text}>Welcome to the Admin page!</h1>
       <p>Please enter your username and password to be granted access.</p>
       <LoginForm />
-      <Appointments />
     </main>
   )
 }
