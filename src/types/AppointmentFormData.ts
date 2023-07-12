63e1f41e2b22ef2a5a6e71212c6dda3da7f97acb
Shraddha <mailmeatshraddha@gmail.com>
2023-07-11 22:56:53 -0400

    change the file extension

---
+++ src/types/AppointmentFormData.ts
@@ -0,0 +1,12 @@
+export type AppointmentFormData = {
+  first_name: string;
+  last_name: string;
+  mobile_phone: string;
+  email: string;
+  dob: string;
+  requested_date: string;
+  requested_time: string;
+  appointment_type: string;
+  description: string;
+  is_emergency: string;
+};
