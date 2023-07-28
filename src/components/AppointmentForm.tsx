6ab4da6c3c86935f8bc709c75ef4653cb0d8046d
Devin Jaggernauth <neexis@gmail.com>
2023-07-28 08:43:53 -0400

    set default values for phone input

--- src/components/AppointmentForm.tsx
+++ src/components/AppointmentForm.tsx
@@ -71,7 +71,7 @@ const AppointmentForm = () => {
   const defaultValues = {
     first_name: '',
     last_name: '',
-    mobile_phone: '',
+    mobile_phone: '+1868',
     email: '',
     requested_date: '',
     requested_time: '',
@@ -188,19 +188,17 @@ const AppointmentForm = () => {
                     <FormLabel>Phone Contact</FormLabel>
                     <FormControl>
                       <PhoneInputWithCountry
-                        className={cn(
-                          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-                        )}
+                        inputComponent={Input}
                         id="mobile_phone"
+                        name="mobile_phone"
                         international={true}
                         addInternationalOption={false}
-                        countryCallingCodeEditable={false}
+                        countryCallingCodeEditable={true}
                         countryOptionsOrder={['TT']}
                         limitMaxLength={true}
                         defaultCountry="TT"
                         control={form.control}
-                        name="mobile_phone"
-                        defaultValue=""
+                        defaultValue="1868"
                         rules={{
                           required: 'Phone number is required.',
                           validate: (value: string) =>
