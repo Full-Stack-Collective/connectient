1bec8b8c329e2d052c940a9381c4261163b50ac0
Devin Jaggernauth <neexis@gmail.com>
2023-07-28 08:13:02 -0400

    style phone input

--- src/components/AppointmentForm.tsx
+++ src/components/AppointmentForm.tsx
@@ -116,6 +116,7 @@ const AppointmentForm = () => {
   return (
     <>
       {isPreviewMode ? (
+        /// Convert to card?
         <>
           <h2>Appointment Preview</h2>
           <p>
@@ -187,6 +188,9 @@ const AppointmentForm = () => {
                     <FormLabel>Phone Contact</FormLabel>
                     <FormControl>
                       <PhoneInputWithCountry
+                        className={cn(
+                          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+                        )}
                         id="mobile_phone"
                         international={true}
                         addInternationalOption={false}
