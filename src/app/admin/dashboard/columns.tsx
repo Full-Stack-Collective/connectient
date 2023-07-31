633c62516373f5295f6023872248f90789a76d15
Shraddha <mailmeatshraddha@gmail.com>
2023-07-31 13:36:43 -0400

    format data in the dashboard

--- src/app/admin/dashboard/columns.tsx
+++ src/app/admin/dashboard/columns.tsx
@@ -26,6 +26,12 @@ export const columns: ColumnDef<Appointment>[] = [
   {
     accessorKey: 'appointment_type',
     header: () => <div className="py-4 text-lg">Appointment Type</div>,
+    cell: ({ row }) => {
+      const appointmentType: string = row.getValue('appointment_type');
+      return appointmentType
+        ? appointmentType.charAt(0).toUpperCase() + appointmentType.slice(1)
+        : 'N/A';
+    },
   },
   {
     accessorKey: 'requested_date',
@@ -38,25 +44,43 @@ export const columns: ColumnDef<Appointment>[] = [
   {
     accessorKey: 'requested_time',
     header: () => <div className="py-4 text-lg">Requested Time</div>,
+    cell: ({ row }) => {
+      const requestedTime: string = row.getValue('requested_time');
+      return requestedTime
+        ? requestedTime.charAt(0).toUpperCase() + requestedTime.slice(1)
+        : 'N/A';
+    },
   },
   {
     accessorKey: 'is_scheduled',
     header: () => <div className="py-4 text-lg">Scheduled</div>,
+    cell: ({ row }) => {
+      const isAppointmentScheduled: boolean = row.getValue('is_scheduled');
+      return isAppointmentScheduled ? '✔' : '✘';
+    },
   },
   {
     accessorKey: 'scheduled_date',
     header: () => <div className="py-4 text-lg">Scheduled Date</div>,
     cell: ({ row }) => {
+      const isAppointmentScheduled: boolean = row.getValue('is_scheduled');
       const dateISOFormat: string = row.getValue('scheduled_date');
-      return new Date(dateISOFormat).toDateString();
+      return isAppointmentScheduled
+        ? new Date(dateISOFormat).toDateString()
+        : '✘';
     },
   },
   {
     accessorKey: 'scheduled_time',
     header: () => <div className="py-4 text-lg">Scheduled Time</div>,
+    cell: ({ row }) => {
+      const isAppointmentScheduled: boolean = row.getValue('is_scheduled');
+      return isAppointmentScheduled ? '✔' : '✘';
+    },
   },
   {
     accessorKey: 'is_cancelled',
     header: () => <div className="py-4 text-lg">Cancelled</div>,
+    cell: ({ row }) => (row.getValue('is_cancelled') ? '✔' : '✘'),
   },
 ];
