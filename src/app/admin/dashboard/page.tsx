726ba21763b934a7887de49bbfb697873f6088ed
Shraddha <mailmeatshraddha@gmail.com>
2023-07-31 18:47:21 -0400

    add page header

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -12,9 +12,15 @@ const getAllAppointments = async () => {
 const AppointmentDemo = async () => {
   const { data }: { data: Appointment[] | null } = await getAllAppointments();
   return (
-    <div className="flex-1 container mx-auto py-10">
+    <main className="flex-1 container mx-auto pt-4 pb-10">
+      <h1 className="mt-8 mb-12 w-full text-4xl font-extrabold tracking-wide leading-2 text-center md:leading-snug">
+        Connectient Control Tower:{' '}
+        <span className="w-full text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-rose-500">
+          Elevating Patient Experiences
+        </span>
+      </h1>
       <DataTable columns={columns} data={data!} />
-    </div>
+    </main>
   );
 };
 
