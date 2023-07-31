db62adacf767861b95eb8d4543cd747f87b58db7
Shraddha <mailmeatshraddha@gmail.com>
2023-07-31 19:08:18 -0400

    refactor table column header

--- src/app/admin/dashboard/columns.tsx
+++ src/app/admin/dashboard/columns.tsx
@@ -4,6 +4,8 @@ import { ColumnDef } from '@tanstack/react-table';
 import { Button } from '@/components/ui/button';
 import { ArrowUpDown } from 'lucide-react';
 
+import { DataTableColumnHeader } from '@/components/DataTableColumnHeader';
+
 export const columns: ColumnDef<Appointment>[] = [
   {
     accessorKey: 'is_emergency',
@@ -11,70 +13,39 @@ export const columns: ColumnDef<Appointment>[] = [
   },
   {
     accessorKey: 'first_name',
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          className="my-4 w-32 text-center"
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-        >
-          First Name
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="First Name" />
+    ),
     cell: ({ row }) => (
       <p className="text-center">{row.getValue('first_name')}</p>
     ),
   },
   {
     accessorKey: 'last_name',
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          className="my-4 w-32 text-center"
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-        >
-          Last Name
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Last Name" />
+    ),
     cell: ({ row }) => (
       <p className="text-center">{row.getValue('last_name')}</p>
     ),
   },
   {
     accessorKey: 'mobile_phone',
-    header: () => {
-      return <p className="py-4 w-32 text-center">Contact</p>;
-    },
+    header: () => <p className="py-4 w-32 text-center">Contact</p>,
     cell: ({ row }) => (
       <p className="text-right">{row.getValue('mobile_phone')}</p>
     ),
   },
   {
     accessorKey: 'email',
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          className="my-4 w-32 text-center"
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-        >
-          Email
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Email" />
+    ),
     cell: ({ row }) => <p className="text-center">{row.getValue('email')}</p>,
   },
   {
     accessorKey: 'appointment_type',
-    header: () => {
-      return <div className="py-4 w-32 text-center">Appointment Type</div>;
-    },
+    header: () => <div className="py-4 w-32 text-center">Appointment Type</div>,
     cell: ({ row }) => {
       const appointmentType: string = row.getValue('appointment_type');
       return (
@@ -88,18 +59,9 @@ export const columns: ColumnDef<Appointment>[] = [
   },
   {
     accessorKey: 'requested_date',
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          className="my-4 w-40 text-center"
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-        >
-          Requested Date
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Requested Date" />
+    ),
     cell: ({ row }) => {
       const dateISOFormat: string = row.getValue('requested_date');
       return (
@@ -109,9 +71,7 @@ export const columns: ColumnDef<Appointment>[] = [
   },
   {
     accessorKey: 'requested_time',
-    header: () => {
-      return <div className="py-4 w-40 text-center">Requested Time</div>;
-    },
+    header: () => <div className="py-4 w-40 text-center">Requested Time</div>,
     cell: ({ row }) => {
       const requestedTime: string = row.getValue('requested_time');
       return (
@@ -125,7 +85,7 @@ export const columns: ColumnDef<Appointment>[] = [
   },
   {
     accessorKey: 'is_scheduled',
-    header: () => <div className="py-4 ">Scheduled</div>,
+    header: () => <div className="py-4 w-20 text-center">Scheduled</div>,
     cell: ({ row }) => {
       const isAppointmentScheduled: boolean = row.getValue('is_scheduled');
       return (
@@ -135,18 +95,9 @@ export const columns: ColumnDef<Appointment>[] = [
   },
   {
     accessorKey: 'scheduled_date',
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          className="my-4 w-40 text-center"
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-        >
-          Scheduled Date
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Scheduled Date" />
+    ),
     cell: ({ row }) => {
       const isAppointmentScheduled: boolean = row.getValue('is_scheduled');
       const dateISOFormat: string = row.getValue('scheduled_date');
@@ -161,9 +112,7 @@ export const columns: ColumnDef<Appointment>[] = [
   },
   {
     accessorKey: 'scheduled_time',
-    header: () => {
-      return <div className="py-4 w-40 text-center">Scheduled Time</div>;
-    },
+    header: () => <div className="py-4 w-40 text-center">Scheduled Time</div>,
     cell: ({ row }) => {
       const isAppointmentScheduled: boolean = row.getValue('is_scheduled');
       return (
@@ -173,7 +122,7 @@ export const columns: ColumnDef<Appointment>[] = [
   },
   {
     accessorKey: 'is_cancelled',
-    header: () => <div className="py-4 ">Cancelled</div>,
+    header: () => <div className="py-4 w-20 text-center">Cancelled</div>,
     cell: ({ row }) => (
       <p className="text-center">{row.getValue('is_cancelled') ? '✔' : '✘'}</p>
     ),
