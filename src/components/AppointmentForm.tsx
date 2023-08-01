f2e458d9d66416440adf26b2da8a1e87ea8bd06c
Devin Jaggernauth <neexis@gmail.com>
2023-08-01 15:19:10 -0400

    refactor preview card as separate component

--- src/components/AppointmentForm.tsx
+++ src/components/AppointmentForm.tsx
@@ -46,18 +46,11 @@ import {
 import { Calendar as CalendarIcon } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import { Checkbox } from './ui/checkbox';
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-  CardContent,
-  CardFooter,
-} from './ui/card';
 
 import { useToast } from '@/components/ui/use-toast';
 import { capitalizeWord } from '@/utils/utils';
 import { ToastAction } from './ui/toast';
+import AppointmentPreview from './AppointmentPreview';
 
 const formSchema = z.object({
   first_name: z
@@ -138,83 +131,11 @@ const AppointmentForm = () => {
   return (
     <>
       {isPreviewMode ? (
-        /// Convert to card?
-        <Card className="my-12 text-base">
-          <CardHeader>
-            <CardTitle>Does this look right?</CardTitle>
-            <CardDescription>Confirm your details to submit</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>
-              <span className="font-semibold">Appointment for: </span>{' '}
-              {createdAppointment?.first_name} {createdAppointment?.last_name}
-            </p>
-          </CardContent>
-          <CardContent>
-            <p>
-              <span className="font-semibold">Email: </span>
-              {createdAppointment?.email}
-            </p>
-          </CardContent>
-          <CardContent>
-            <p>
-              <span className="font-semibold">Phone: </span>{' '}
-              {createdAppointment?.mobile_phone}
-            </p>
-          </CardContent>
-          <CardContent>
-            <p>
-              <span className="font-semibold">Requested Date: </span>
-              {createdAppointment?.requested_date?.toLocaleDateString('en-us', {
-                weekday: 'long',
-                year: 'numeric',
-                month: 'short',
-                day: 'numeric',
-              })}
-            </p>
-          </CardContent>
-
-          <CardContent>
-            <p>
-              <span className="font-semibold">Preferred Time: </span>
-              {createdAppointment?.requested_time}
-            </p>
-          </CardContent>
-          <CardContent>
-            <p>
-              <span className="font-semibold">Procedure Type: </span>
-              {createdAppointment?.appointment_type &&
-                capitalizeWord(createdAppointment?.appointment_type)}
-            </p>
-          </CardContent>
-
-          {createdAppointment?.description ? (
-            <CardContent>
-              <p>
-                <span className="font-semibold">Additional Details: </span>
-                {capitalizeWord(createdAppointment.description)}
-              </p>
-            </CardContent>
-          ) : null}
-
-          {createdAppointment?.is_emergency ? (
-            <CardContent>
-              <p>This is an emergency</p>
-            </CardContent>
-          ) : null}
-
-          <CardFooter className="flex justify-around my-4">
-            {' '}
-            <Button variant="outline" onClick={handleGoBack}>
-              Go Back{' '}
-            </Button>
-            <Button
-              onClick={() => handleConfirmAppointment(createdAppointment!)}
-            >
-              Submit
-            </Button>
-          </CardFooter>
-        </Card>
+        <AppointmentPreview
+          createdAppointment={createdAppointment!}
+          handleConfirmAppointment={handleConfirmAppointment}
+          handleGoBack={handleGoBack}
+        />
       ) : (
         <div className="my-8">
           <h2 className="font-semibold">
