92e029e724b63ecb45c02e77cb3e7391e2f24233
Shraddha <mailmeatshraddha@gmail.com>
2023-08-13 23:40:48 -0400

    add tabs to view, schedule & cancel

--- src/components/AdminAppointmentDetailsPopup.tsx
+++ src/components/AdminAppointmentDetailsPopup.tsx
@@ -5,6 +5,7 @@ import {
   DialogTitle,
 } from '@/components/ui/dialog';
 import { Badge } from '@/components/ui/badge';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 type AppointmentDescriptionPopupProps = {
   open: boolean;
@@ -69,104 +70,137 @@ const AdminAppointmentDetailsPopup = ({
   return (
     <Dialog open={open} onOpenChange={onClose}>
       <DialogContent className="w-full max-w-[375px] sm:max-w-[540px] text-base rounded-md">
-        <DialogHeader>
-          <DialogTitle className="py-2 text-center text-2xl border-b">
-            <span className="w-full text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-rose-500">
-              Patient Chronicles
-            </span>{' '}
-            <br />A Peek into the Appointment
-          </DialogTitle>
-        </DialogHeader>
-        <div className="px-2 flex gap-4">{renderStatusBadges()}</div>
-        <div className="grid gap-2 py-2 px-2">
-          <div className="flex gap-4">
-            <h3 className="font-bold">Name :</h3>
-            <p>{`${first_name} ${last_name}`}</p>
-          </div>
-          <div className="flex gap-4">
-            <h3 className="font-bold">Date of Birth: </h3>
-            <p>
-              {new Date(dob!).toLocaleDateString('en-us', {
-                weekday: 'long',
-                year: 'numeric',
-                month: 'short',
-                day: 'numeric',
-              })}
-            </p>
-          </div>
-          <div className="flex gap-4">
-            <h3 className="font-bold">Email: </h3>
-            <p>
-              <a
-                href={`mailto:${email}`}
-                className="no-underline transition-all hover:underline ease-in-out"
-              >
-                {email}
-              </a>
-            </p>
-          </div>
-          <div className="flex gap-4">
-            <h3 className="font-bold">Contact Number: </h3>
-            <p>
-              <a
-                href={`tel:${mobile_phone}`}
-                className="no-underline transition-all hover:underline ease-in-out"
-              >
-                {formatMobilePhone(mobile_phone)}
-              </a>
-            </p>
-          </div>
-          <div className="flex gap-4">
-            <h3 className="font-bold">Type of Appointment: </h3>
-            <p>
-              {appointment_type
-                ? `${appointment_type[0].toUpperCase()}${appointment_type.slice(
-                    1,
-                  )}`
-                : 'N/A'}
-            </p>
-          </div>
-          <div className="flex flex-col gap-1">
-            <h3 className="font-bold">Description: </h3>
-            <p className="ml-2">{description ? description : 'N/A'}</p>
-          </div>
-          <div className="flex gap-4">
-            <h3 className="font-bold">Requested Date: </h3>
-            <p>
-              {new Date(requested_date!).toLocaleDateString('en-us', {
-                weekday: 'long',
-                year: 'numeric',
-                month: 'short',
-                day: 'numeric',
-              })}
-            </p>
-          </div>
-          <div className="flex gap-4">
-            <h3 className="font-bold">Requested Time: </h3>
-            <p>
-              {requested_time
-                ? `${requested_time[0].toUpperCase()}${requested_time.slice(1)}`
-                : ''}
-            </p>
-          </div>
-          <div className="flex gap-4">
-            <h3 className="font-bold">Scheduled Date: </h3>
-            <p>
-              {is_scheduled
-                ? new Date(scheduled_date!).toLocaleDateString('en-us', {
+        <Tabs defaultValue="view-details" className="">
+          <TabsList>
+            <TabsTrigger value="view-details">View Details</TabsTrigger>
+            <TabsTrigger value="schedule">Schedule</TabsTrigger>
+            <TabsTrigger value="cancel">Cancel</TabsTrigger>
+          </TabsList>
+          <TabsContent value="view-details">
+            <DialogHeader>
+              <DialogTitle className="py-2 text-center text-2xl border-b">
+                <span className="w-full text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-rose-500">
+                  Patient Chronicles
+                </span>{' '}
+                <br />A Peek into the Appointment
+              </DialogTitle>
+            </DialogHeader>
+            <div className="mt-4 px-2 flex gap-4">{renderStatusBadges()}</div>
+            <div className="grid gap-2 py-2 px-2">
+              <div className="flex gap-4">
+                <h3 className="font-bold">Name :</h3>
+                <p>{`${first_name} ${last_name}`}</p>
+              </div>
+              <div className="flex gap-4">
+                <h3 className="font-bold">Date of Birth: </h3>
+                <p>
+                  {new Date(dob!).toLocaleDateString('en-us', {
                     weekday: 'long',
                     year: 'numeric',
                     month: 'short',
                     day: 'numeric',
-                  })
-                : 'N/A'}
-            </p>
-          </div>
-          <div className="flex gap-4">
-            <h3 className="font-bold">Who scheduled the appointment?: </h3>
-            <p>{scheduled_by ?? 'N/A'}</p>
-          </div>
-        </div>
+                  })}
+                </p>
+              </div>
+              <div className="flex gap-4">
+                <h3 className="font-bold">Email: </h3>
+                <p>
+                  <a
+                    href={`mailto:${email}`}
+                    className="no-underline transition-all hover:underline ease-in-out"
+                  >
+                    {email}
+                  </a>
+                </p>
+              </div>
+              <div className="flex gap-4">
+                <h3 className="font-bold">Contact Number: </h3>
+                <p>
+                  <a
+                    href={`tel:${mobile_phone}`}
+                    className="no-underline transition-all hover:underline ease-in-out"
+                  >
+                    {formatMobilePhone(mobile_phone)}
+                  </a>
+                </p>
+              </div>
+              <div className="flex gap-4">
+                <h3 className="font-bold">Type of Appointment: </h3>
+                <p>
+                  {appointment_type
+                    ? `${appointment_type[0].toUpperCase()}${appointment_type.slice(
+                        1,
+                      )}`
+                    : 'N/A'}
+                </p>
+              </div>
+              <div className="flex flex-col gap-1">
+                <h3 className="font-bold">Description: </h3>
+                <p className="ml-2">{description ? description : 'N/A'}</p>
+              </div>
+              <div className="flex gap-4">
+                <h3 className="font-bold">Requested Date: </h3>
+                <p>
+                  {new Date(requested_date!).toLocaleDateString('en-us', {
+                    weekday: 'long',
+                    year: 'numeric',
+                    month: 'short',
+                    day: 'numeric',
+                  })}
+                </p>
+              </div>
+              <div className="flex gap-4">
+                <h3 className="font-bold">Requested Time: </h3>
+                <p>
+                  {requested_time
+                    ? `${requested_time[0].toUpperCase()}${requested_time.slice(
+                        1,
+                      )}`
+                    : ''}
+                </p>
+              </div>
+              <div className="flex gap-4">
+                <h3 className="font-bold">Scheduled Date: </h3>
+                <p>
+                  {is_scheduled
+                    ? new Date(scheduled_date!).toLocaleDateString('en-us', {
+                        weekday: 'long',
+                        year: 'numeric',
+                        month: 'short',
+                        day: 'numeric',
+                      })
+                    : 'N/A'}
+                </p>
+              </div>
+              <div className="flex gap-4">
+                <h3 className="font-bold">Who scheduled the appointment?: </h3>
+                <p>{scheduled_by ?? 'N/A'}</p>
+              </div>
+            </div>
+          </TabsContent>
+          <TabsContent value="schedule">
+            <DialogHeader>
+              <DialogTitle className="py-2 text-center text-2xl border-b">
+                <span className="w-full text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-rose-500">
+                  Appointment Artistry
+                </span>{' '}
+                <br />
+                Crafting a Seamless Encounter
+              </DialogTitle>
+            </DialogHeader>
+          </TabsContent>
+          <TabsContent value="cancel">
+            <DialogHeader>
+              <DialogTitle className="py-2 text-center text-2xl border-b">
+                <span className="w-full text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-rose-500">
+                  Shifting Sands
+                </span>{' '}
+                <br />
+                Facilitating Cancellations
+              </DialogTitle>
+            </DialogHeader>
+          </TabsContent>
+        </Tabs>
       </DialogContent>
     </Dialog>
   );
