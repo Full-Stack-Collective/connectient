b7df1abb64adc2c70002b85e6c79f0e483f76b34
Devin Jaggernauth <neexis@gmail.com>
2023-08-18 15:37:46 -0400

    add footer padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,27 +7,30 @@ type FooterProps = {
     link: string;
   }[];
   logoLink: string;
+  isUserPage: boolean;
 };
 
-const Footer = ({ menuList, logoLink }: FooterProps) => {
+const Footer = ({ menuList, logoLink, isUserPage = false }: FooterProps) => {
   return (
     <footer className="bg-background text-base flex flex-col gap-4 md:gap-2">
       <div className="m-auto max-w-7xl w-full pt-6 md:pt-4 pb-1 px-4 flex flex-col gap-4 md:flex-row">
-        <div className="w-full flex flex-col gap-2 items-center md:items-start">
-          <p className="-mx-4">Powered by</p>
+        <div className="w-full flex flex-col gap-2 px-5 items-center md:items-start">
+          {isUserPage ? <p className="-mx-4">Powered by</p> : null}
           <Logo link={logoLink} />
         </div>
-        <div className="min-w-fit flex flex-col gap-2 items-center md:items-start">
-          {menuList?.map((menuItem) => (
-            <Link
-              key={menuItem.name}
-              href={menuItem.link}
-              className="hover:underline transition-all ease-in-out"
-            >
-              {menuItem.name}
-            </Link>
-          ))}
-        </div>
+        {!isUserPage ? (
+          <div className="min-w-fit flex flex-col gap-2 items-center md:items-start">
+            {menuList?.map((menuItem) => (
+              <Link
+                key={menuItem.name}
+                href={menuItem.link}
+                className="hover:underline transition-all ease-in-out"
+              >
+                {menuItem.name}
+              </Link>
+            ))}
+          </div>
+        ) : null}
       </div>
       <p className="pb-2 text-center text-sm text-muted-foreground opacity-50">
         &copy;{new Date().getFullYear()} Full Stack Collective
