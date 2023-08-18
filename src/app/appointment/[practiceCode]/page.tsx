520777970d36b5b1dfa043d506ea3ab63602e2dd
Devin Jaggernauth <neexis@gmail.com>
2023-08-18 11:10:32 -0400

    redirect user if invalid appointment url

--- src/app/appointment/[practiceCode]/page.tsx
+++ src/app/appointment/[practiceCode]/page.tsx
@@ -1,6 +1,7 @@
 import AppointmentForm from '@/components/AppointmentForm';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
+import Image from 'next/image';
 import { redirect } from 'next/navigation';
 
 const supabase = createServerComponentClient<Database>({ cookies });
@@ -14,7 +15,7 @@ export default async function Appointment({
 
   const { data } = await supabase
     .from('Practice')
-    .select('id, name')
+    .select('id, name, logo')
     .eq('practice_code', practiceCode);
 
   if (!data || data?.length < 1) redirect('/');
@@ -24,10 +25,18 @@ export default async function Appointment({
   return (
     <>
       <main className="flex-1 flex flex-col gap-2 justify-center items-center">
-        {name ? (
-          <h1 className="text-2xl font-semibold">Welcome to {name}</h1>
-        ) : null}
-
+        <div className="mx-auto">
+          {name ? (
+            <h1 className="text-2xl font-semibold">Welcome to {name}</h1>
+          ) : null}
+          {logo ? (
+            <img
+              src={logo}
+              className="h-28 w-28 object-contain mx-auto"
+              alt={`Logo for a dental practice called ${name}`}
+            />
+          ) : null}
+        </div>
         <AppointmentForm practiceId={id} />
       </main>
     </>
