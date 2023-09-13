48d67b162e31480390dc3e19ab770bb1c4582713
nuriya <nuriya_a@yahoo.com>
2023-09-13 17:52:25 -0400

    feature 12 hour format time

--- src/components/AdminAppointmentDetailsPopup.tsx
+++ src/components/AdminAppointmentDetailsPopup.tsx
@@ -141,11 +141,12 @@ const AdminAppointmentDetailsPopup = ({
       const hh = Math.floor(startHourInMinute / 60); // getting hours of day in 0-24 format
 
       const mm = startHourInMinute % 60; // getting minutes of the hour in 0-55 format
+      const ampm = hh >= 12 ? 'PM' : 'AM';
+      const hh12 = hh === 0 ? 12 : hh > 12 ? hh - 12 : hh;
 
-      times[i] =
-        ('0' + (hh % 24).toString()).slice(-2) +
-        ':' +
-        ('0' + mm.toString()).slice(-2);
+      times[i] = `${('0' + hh12.toString()).slice(-2)}:${(
+        '0' + mm.toString()
+      ).slice(-2)} ${ampm}`;
 
       startHourInMinute = startHourInMinute + interval;
     }
