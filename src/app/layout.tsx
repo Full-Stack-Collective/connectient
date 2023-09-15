66efe16eeb60110904d5f210cc7848495bd15c0e
Shraddha <mailmeatshraddha@gmail.com>
2023-09-14 21:55:33 -0400

    add font optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Lato } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
 import './globals.css';
@@ -7,10 +8,17 @@ export const metadata = {
   description: 'Appointments Made Easy',
 };
 
+const lato = Lato({
+  weight: ['300', '400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-lato',
+});
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
-      <body className="font-sans-serif text-lg">
+    <html lang="en" className={`${lato.variable}`}>
+      <body className="text-lg">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className="min-h-screen flex flex-col">{children}</div>
           <Toaster />
