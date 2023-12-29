1033ddf963749fd7297a79d7bd65c5b4247cd139
Devin Jaggernauth <neexis@gmail.com>
2023-12-29 08:55:19 -0400

    set up generate function:

--- src/config/emailContent.ts
+++ src/config/emailContent.ts
@@ -104,3 +104,16 @@ export const generateConfirmationEmailContent = (
     }</p>}<img alt="${practice.name!} logo"src=${practice.logo!} style=width:200px></div><table border=0 cellpadding=0 cellspacing=0 width=100%><tr><td class=section-padding style="padding:10px 15px 30px 15px"align=center bgcolor=#ffffff><table border=0 cellpadding=0 cellspacing=0 width=100% class=responsive-table style=max-width:500px><tr><td><table border=0 cellpadding=0 cellspacing=0 width=100%><tr><td><table border=0 cellpadding=0 cellspacing=0 width=100%><tr><td class="message-content padding"style=padding:0;font-size:16px;line-height:25px;color:#232323><h2>Appointment Confirmation Information</h2><div class=form-container>${htmlData}</div></table></table></table></table>`,
   };
 };
+export const generatePracticeNotificationEmail = (
+  data: ConfirmationEmailData,
+  practice: PracticeEmailData,
+) => {
+  delete data.practice_id;
+
+  return {
+    to: practice.email,
+    subject: `New Appointment Request`,
+    text: 'You have a new appointment!',
+    html: `<!doctypehtml><title></title><meta charset=utf-8><meta content="width=device-width,initial-scale=1"name=viewport><meta content="IE=edge"http-equiv=X-UA-Compatible><style>a,body,table,td{-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%}table{border-collapse:collapse!important}body{height:100%!important;margin:0!important;padding:0!important;width:100%!important}@media screen and (max-width:525px){.wrapper{width:100%!important;max-width:100%!important}.responsive-table{width:100%!important}.padding{padding:10px 5% 15px 5%!important}.section-padding{padding:0 15px 50px 15px!important}}.form-container{margin-bottom:24px;padding:20px;border:1px dashed #ccc}.form-heading{color:#2a2a2a;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;font-weight:400;text-align:left;line-height:20px;font-size:18px;margin:0 0 8px;padding:0}.form-answer{color:#2a2a2a;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;font-weight:300;text-align:left;line-height:20px;font-size:1.2rem;margin:0 0 24px;padding:0}div[style*='margin: 16px 0;']{margin:0!important}</style><body style=margin:0!important;padding:0!important;background:#fff><div style=display:none;font-size:1px;color:#fefefe;line-height:1px;max-height:0;max-width:0;opacity:0;overflow:hidden></div><div style="margin:20px 50px;font-size:1.1rem"><p>Hello!,<p>An appointment has been requested via your Connectient booking page. Please login to your <a href="https://connectient.co/admin/login" target="_blank">Dashboard</a> to contact the patient!<p style=margin:0>The Connectient Team</p>}<img alt="Connectient logo"src="https://vvlzpciuhggoletuwmqf.supabase.co/storage/v1/object/public/Practice%20Media/connectient-logo.png" style=width:300px></div>`,
+  };
+};
